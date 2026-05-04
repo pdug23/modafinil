b31cc1793e1d5c74fec6243f103c3511d32ad7f2
pdug <pdug00@gmail.com>
2026-05-04 18:56:53 +0100

    Initial Commit

---
+++ modafinil/modafinilApp.swift
@@ -0,0 +1,17 @@
+//
+//  modafinilApp.swift
+//  modafinil
+//
+//  Created by Patrick Duggan on 04/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct modafinilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
